3634a40567eba4a7b9abc7a7700a3c9eba293b3b
oleksandr-chernetskyi <liero2d@gmail.com>
2025-10-27 13:12:11 +0200

    test: add unit and controller tests for Data API (service-b)

--- service-b/src/main/java/dataapi/serviceb/dto/TransformResponseDto.java
+++ service-b/src/main/java/dataapi/serviceb/dto/TransformResponseDto.java
@@ -2,9 +2,11 @@ package dataapi.serviceb.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class TransformResponseDto {
     private String result;
 }
--- service-b/src/test/java/dataapi/serviceb/service/impl/TransformServiceImplTest.java
+++ service-b/src/test/java/dataapi/serviceb/service/impl/TransformServiceImplTest.java
@@ -1,4 +1,44 @@
-import static org.junit.jupiter.api.Assertions.*;
+package dataapi.serviceb.service.impl;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+import dataapi.serviceb.dto.TransformRequestDto;
+import dataapi.serviceb.dto.TransformResponseDto;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 class TransformServiceImplTest {
-  
+    private TransformServiceImpl transformService;
+
+    @BeforeEach
+    void setUp() {
+        transformService = new TransformServiceImpl();
+    }
+
+    @Test
+    @DisplayName("transformResponseDto_ValidInput_ReturnsTransformedResponse")
+    void transformResponseDtoValidInputReturnsTransformedResponse() {
+        TransformRequestDto transformRequestDto = new TransformRequestDto();
+        transformRequestDto.setText("hello");
+
+        TransformResponseDto transformResponseDto = transformService
+                .transformResponseDto(transformRequestDto);
+
+        assertThat(transformResponseDto).isNotNull();
+        assertThat(transformResponseDto.getResult()).isEqualTo("OLLEH");
+    }
+
+    @Test
+    @DisplayName("transformResponseDto_NullText_ThrowsNullPointerException")
+    void transformResponseDtoNullTextThrowsNullPointerException() {
+        TransformRequestDto transformRequestDto = new TransformRequestDto();
+        transformRequestDto.setText(null);
+
+        assertThatThrownBy(() -> transformService
+                .transformResponseDto(transformRequestDto))
+                .isInstanceOf(NullPointerException.class)
+                .hasMessageContaining("Cannot invoke");
+    }
 }
